c525a780c7e47ad9f57148b1ea1dba05d277a2e5
unknown <Mikolaj.Slon@wp.pl>
2018-05-09 02:48:14 +0200

    measurement methods

---
+++ StringDistance.py
@@ -0,0 +1,22 @@
+import numpy
+
+class StringDistance(object):
+
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+    def calculate(self):
+        width = len(self.source)
+        heigth = len(self.target)
+        table = numpy.zeros((width, heigth))
+        for i in range(width):
+            for j in range(heigth):
+                cost = 1
+                if self.source[i] == self.target[j]:
+                    cost = 0
+                above = table[i-1, j] + 1
+                left = table[i, j-1] + 1
+                diagonal = table[i-1, j-1] + cost
+                table[i, j] = min(above, left, diagonal)
+        return table[width -1][heigth -1]
